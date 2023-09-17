1965be4ca7ee4566f1a20e913fa4db7ffdcb6fbf
logankud <logankud@protonmail.com>
2023-09-17 09:20:34 -0400

    attempting to run a full backfill

--- prymal/data_pipelines/shopify/order_data_backfill/shopify_extraction_backfill.py
+++ prymal/data_pipelines/shopify/order_data_backfill/shopify_extraction_backfill.py
@@ -30,9 +30,9 @@ SHOPIFY_API_PASSWORD = os.environ['SHOPIFY_API_PASSWORD']
 
 API_VERSION = '2021-07'
 
-START_DATE = pd.to_datetime('2023-09-01').strftime('%Y-%m-%d 00:00:00')
-# END_DATE = pd.to_datetime('2023-08-31').strftime('%Y-%m-%d 23:59:59')
-END_DATE = pd.to_datetime(pd.to_datetime('today') - timedelta(1)).strftime('%Y-%m-%d 23:59:59')
+START_DATE = pd.to_datetime('2018-01-01').strftime('%Y-%m-%d 00:00:00')
+END_DATE = pd.to_datetime('2023-07-31').strftime('%Y-%m-%d 23:59:59')
+# END_DATE = pd.to_datetime(pd.to_datetime('today') - timedelta(1)).strftime('%Y-%m-%d 23:59:59')
 
 SHOPIFY_URL = f'https://{SHOPIFY_API_KEY}:{SHOPIFY_API_PASSWORD}@prymal-coffee-creamer.myshopify.com/admin/api/{API_VERSION}'
 
