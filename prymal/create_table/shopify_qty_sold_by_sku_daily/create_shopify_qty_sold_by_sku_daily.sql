4bbbb7616459bf91afc599ec1ce49fa0dcd99be7
logankud <logankud@protonmail.com>
2023-09-17 20:21:57 -0400

    fixed syntax for partition column name

--- prymal/create_table/shopify_qty_sold_by_sku_daily/create_shopify_qty_sold_by_sku_daily.sql
+++ prymal/create_table/shopify_qty_sold_by_sku_daily/create_shopify_qty_sold_by_sku_daily.sql
@@ -5,8 +5,10 @@ CREATE EXTERNAL TABLE IF NOT EXISTS "prymal-analytics"."shopify_qty_sold_by_sku_
 , title VARCHAR "Name of product from Shopify"
 , qty_sold INT "Total quantity of sku sold on order_date"
 , sku_name VARCHAR "Mapped name of the sku (from Prymal's skus_shopify table)"
-
 )
- PARTITIONED BY ('parttition_date')
+ PARTITIONED BY 
+(
+partition_date DATE "Order date that the sku was sold" 
+)
  STORED AS CSV
  LOCATION 's3://prymal-analytics/shopify/shopify_qty_sold_by_sku_daily/'
