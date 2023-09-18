2b707b0c2fc8ec856ccb31802134d489e3a16a70
logankud <logankud@protonmail.com>
2023-09-17 20:17:55 -0400

    initial commit for create_table shopify_qty_sold_by_sku_daily

---
+++ prymal/create_table/shopify_qty_sold_by_sku_daily/create_shopify_qty_sold_by_sku_daily.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS "prymal-analytics"."shopify_qty_sold_by_sku_daily"
+(
+ order_date DATE "Date that the sku was sold"
+, sku VARCHAR "SKU (from Shopify) being sold"
+, title VARCHAR "Name of product from Shopify"
+, qty_sold INT "Total quantity of sku sold on order_date"
+, sku_name VARCHAR "Mapped name of the sku (from Prymal's skus_shopify table)"
+
+)
+ PARTITIONED BY ('parttition_date')
+ STORED AS CSV
+ LOCATION 's3://prymal-analytics/shopify/shopify_qty_sold_by_sku_daily/'
