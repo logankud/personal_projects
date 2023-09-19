ad7d65de476ca1b1e8e504e08e2c55603d222296
logankud <logankud@protonmail.com>
2023-09-19 19:16:38 -0400

    updated table schema

--- prymal/create_table/shopify_qty_sold_by_sku_daily/create_shopify_qty_sold_by_sku_daily.sql
+++ prymal/create_table/shopify_qty_sold_by_sku_daily/create_shopify_qty_sold_by_sku_daily.sql
@@ -1,9 +1,7 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS shopify_qty_sold_by_sku_daily(
  order_date DATE
-, sku STRING 
-, title STRING
-, qty_sold INT
 , sku_name STRING
+, qty_sold INT
 
 )
 PARTITIONED BY 
