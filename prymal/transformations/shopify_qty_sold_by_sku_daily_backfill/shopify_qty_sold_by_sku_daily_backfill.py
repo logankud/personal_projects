b636c4e8a276962d74fe4db8f3c1a4acf05a3c15
logankud <logankud@protonmail.com>
2024-01-18 19:24:55 -0500

    re-running backfill after deleting

--- prymal/transformations/shopify_qty_sold_by_sku_daily_backfill/shopify_qty_sold_by_sku_daily_backfill.py
+++ prymal/transformations/shopify_qty_sold_by_sku_daily_backfill/shopify_qty_sold_by_sku_daily_backfill.py
@@ -452,7 +452,7 @@ def run_athena_query_no_results(query:str, database: str):
 QUERY_STR = read_query_to_string(path=QUERY_PATH)
 
 
-START_DATE = pd.to_datetime('2023-06-01').strftime('%Y-%m-%d')
+START_DATE = pd.to_datetime('2020-01-01').strftime('%Y-%m-%d')
 END_DATE = pd.to_datetime('today').strftime('%Y-%m-%d')
 
 while START_DATE <= END_DATE:
